5e76f6a2255ddbdf52a9e861bfc297d5fc091f8a
Mike Pawlak <mpawlak719@gmail.com>
2020-10-10 18:23:57 -0500

    jira c,m, and s

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { JiraModule } from './jira/jira/jira.module';
+import { JiraModule } from './jira/jira.module';
+import { JiraController } from './jira/jira.controller';
+import { JiraModule } from './jira/jira.module';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [AppController],
+  imports: [HealthModule, JiraModule],
+  controllers: [AppController, JiraController],
   providers: [AppService],
 })
 export class AppModule {}
